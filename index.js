53dd2f90f0178cbe5630cae66a8e4d90c7ba461d
Johnhughes814 <8hughes.john@gmail.com>
2022-03-07 17:12:32 -0600

    starter files

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+
+
+const questions = [
+    {
+        type:'input',
+        name:'username',
+        message: "Enter your github username: "
+    },
+    {
+        type:'input',
+        name:'projectTitle',
+        message: "What is the title of your project: "
+    },
+    {
+        type:'input',
+        name:'description',
+        message: "Include a description of your project: "
+    },
+    {
+        type:'input',
+        name:'installation',
+        message: "Specify the commands to install and run the project: "
+    },
+    {
+        type:'input',
+        name:'usage',
+        message: "Enter your first and las: "
+    },
+    {
+        type:'input',
+        name:'fullName',
+        message: "Enter your first and last name: "
+    },
+    {
+        type:'input',
+        name:'fullName',
+        message: "Enter your first and last name: "
+    },
+    {
+        type:'input',
+        name:'fullName',
+        message: "Enter your first and last name: "
+    },
+    {
+        type:'list',
+        name:'license',
+        message: "Choose the most suitabel license for this project: ",
+        choices: [
+            "MIT",
+            "GPL 3.0",
+            "Apache 2.0",
+            "Other",
+            "None"
+        ]
+    },
+    {
+        type:"input",
+        name: "tests"
+    }
+
+]
---
+++ utils/markdownTemplate.js
@@ -0,0 +1,26 @@
+function markdownTemplate (answers, response){
+    return `
+# ${answers.title} 
+
+![alt](url)
+
+<img src="url" alt="alt"/>
+    
+    
+    
+    
+    
+    
+    `
+} 
+
+
+
+
+
+// ## Contributing
+
+// There are many ways in which you can participate in this project, for example:
+
+// * [Submit bugs and feature requests](https://github.com/Johnhughes814/markdown-maker/issues), and help us verify as they are checked in
+// * Review [source code changes](https://github.com/Johnhughes814/markdown-maker/pulls)
